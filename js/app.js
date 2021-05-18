99ebd393442d21b8a4639ceb08e18e90db633237
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2021-05-18 16:26:24 -0300

    Hice el codigo mas legible agregando mas funciones y ahora solo se muestra un unico mensaje de error

--- js/app.js
+++ js/app.js
@@ -1,35 +1,71 @@
 // Funciones
+const iniciarApp = () => {
+	resetFormulario();
+	cargarEventListeners();
+};
+const cargarEventListeners = () => {
+	window.addEventListener("load", iniciarApp);
+	email.addEventListener("blur", validarCampo);
+	asunto.addEventListener("blur", validarCampo);
+	mensaje.addEventListener("blur", validarCampo);
+	btnReset.addEventListener("click", resetFormulario);
+	btnSubmit.addEventListener("click", enviarEmail);
+};
+const validarCampo = (e) => {
+	const tagInput = e.target;
+	if (e.target.value === "") {
+		msjError("Todos los campos son obligatorios", tagInput);
+	} else if (e.target.type === "email") {
+		if (validarEmail(e.target.value)) {
+			bordeVerde(tagInput);
+		} else {
+			msjError(`Email "${e.target.value}" no es valido`, tagInput);
+		}
+	} else {
+		bordeVerde(tagInput);
+	}
+	validarCampos(e);
+};
 const validarEmail = (email) => {
 	const regex =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return regex.test(email);
 };
-const agregarBorde = (tipo, input) => {
-	tipo = tipo.toLowerCase();
-	if (tipo === "error") {
-		input.classList.remove("borde-verde");
-		input.classList.add("borde-rojo");
+const bordeRojo = (tagInput) => {
+	tagInput.classList.remove("borde-verde");
+	tagInput.classList.add("borde-rojo");
+};
+const bordeVerde = (tagInput) => {
+	tagInput.classList.remove("borde-rojo");
+	tagInput.classList.add("borde-verde");
+};
+const msjError = (msj, tagInput = undefined) => {
+	if (tagInput !== undefined) {
+		bordeRojo(tagInput);
 	}
-	if (tipo === "correcto") {
-		input.classList.remove("borde-rojo");
-		input.classList.add("borde-verde");
+	const div = document.createElement("div");
+	div.classList.add("msj-error");
+	div.textContent = msj;
+	if (main.children[1]) {
+		main.removeChild(main.children[1]);
+		main.appendChild(div);
+	} else {
+		main.appendChild(div);
 	}
 };
-const mostrarMsj = (tipo, msj) => {
-	const div = document.createElement("div");
-	if (tipo === "error") {
-		div.classList.add("msj-error");
-		div.textContent = msj;
-	}
-	if (tipo === "correcto") {
-		div.classList.add("msj-correcto");
-		div.textContent = msj;
-	}
-
-	main.appendChild(div);
-	setTimeout(() => {
+const borrarMsjError = () => {
+	if (main.children[1]) {
 		main.removeChild(main.children[1]);
-	}, 3000);
+	}
+};
+const msjCorrecto = (msj, tagInput = undefined) => {
+	if (tagInput !== undefined) {
+		bordeVerde(tagInput);
+	}
+	const div = document.createElement("div");
+	div.classList.add("msj-correcto");
+	div.textContent = msj;
+	main.appendChild(div);
 };
 const resetFormulario = () => {
 	email.classList.remove("borde-verde", "borde-rojo");
@@ -38,99 +74,66 @@ const resetFormulario = () => {
 	email.value = "";
 	asunto.value = "";
 	mensaje.value = "";
-	bgSubmit();
+	borrarMsjError();
+	disableSubmit();
 };
-const bgSubmit = () => {
+const enableSubmit = () => {
+	console.log("enable submit");
+	const submit = document.querySelector(".submit-input");
+	submit.disabled = false;
+	submit.classList.remove("cursor-not-allowed");
+	submit.classList.add("cursor-allowed");
+	const divSubmit = submit.parentElement;
+	divSubmit.classList.remove("opacity-50");
+};
+const disableSubmit = () => {
+	const submit = document.querySelector(".submit-input");
+	submit.disabled = true;
+	submit.classList.remove("cursor-allowed");
+	submit.classList.add("cursor-not-allowed");
+	const divSubmit = submit.parentElement;
+	divSubmit.classList.add("opacity-50");
+};
+const validarCampos = (e) => {
+	e.preventDefault();
 	if (
 		validarEmail(email.value) &&
 		asunto.value !== "" &&
 		mensaje.value !== ""
 	) {
-		const submit = formulario.querySelector(".submit-input");
-		submit.classList.remove("btn-disable");
-		submit.classList.add("btn-allowed");
-		const divSubmit = submit.parentElement;
-		divSubmit.classList.remove("bg-btn-disable");
-		divSubmit.classList.add("bg-btn-allowed");
+		enableSubmit();
+		borrarMsjError();
 	} else {
-		const submit = formulario.querySelector(".submit-input");
-		submit.classList.remove("btn-allowed");
-		submit.classList.add("btn-disable");
-		const divSubmit = submit.parentElement;
-		divSubmit.classList.remove("bg-btn-allowed");
-		divSubmit.classList.add("bg-btn-disable");
+		disableSubmit();
 	}
 };
-const validarCampos = (e) => {
+const enviarEmail = (e) => {
 	e.preventDefault();
-	if (e.target.classList.contains("submit-input")) {
-		if (
-			validarEmail(email.value) &&
-			asunto.value !== "" &&
-			mensaje.value !== ""
-		) {
-			const divSpinner = document.createElement("div");
-			divSpinner.classList.add("spinner");
-			main.appendChild(divSpinner);
+	if (
+		validarEmail(email.value) &&
+		asunto.value !== "" &&
+		mensaje.value !== ""
+	) {
+		const divSpinner = document.createElement("div");
+		divSpinner.classList.add("spinner");
+		main.appendChild(divSpinner);
+		setTimeout(() => {
+			main.removeChild(main.children[1]);
+			msjCorrecto("Mensaje enviado correctamente");
 			setTimeout(() => {
-				main.removeChild(main.children[1]);
-				mostrarMsj("correcto", "Email enviado correctamente");
-				setTimeout(() => {
-					resetFormulario();
-				}, 2000);
-			}, 1500);
-		} else {
-			mostrarMsj("error", "Todos los campos son obligatorios");
-		}
+				resetFormulario();
+			}, 2000);
+		}, 1500);
 	}
 };
 // Variables
 const main = document.querySelector(".main");
-const formulario = document.querySelector(".form");
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
 const mensaje = document.querySelector("#mensaje");
 const btnReset = document.querySelector(".reset-input");
+const btnSubmit = document.querySelector(".submit-input");
 
-const cargarEventListeners = () => {
-	window.addEventListener("load", bgSubmit);
-	email.addEventListener("blur", (e) => {
-		const input = e.target;
-		const valueInput = e.target.value;
-		if (valueInput === "") {
-			agregarBorde("error", input);
-			mostrarMsj("error", `El email esta vacio`);
-		} else if (!validarEmail(valueInput)) {
-			agregarBorde("error", input);
-			mostrarMsj("error", `Email "${valueInput}" no es valido`);
-		} else {
-			agregarBorde("correcto", input);
-		}
-		bgSubmit();
-	});
-	asunto.addEventListener("blur", (e) => {
-		const input = e.target;
-		const valueInput = e.target.value;
-		if (valueInput === "") {
-			agregarBorde("error", input);
-			mostrarMsj("error", "El asunto esta vacio");
-		} else {
-			agregarBorde("correcto", input);
-		}
-		bgSubmit();
-	});
-	mensaje.addEventListener("blur", (e) => {
-		const input = e.target;
-		const valueInput = e.target.value;
-		if (valueInput === "") {
-			agregarBorde("error", input);
-			mostrarMsj("error", "El mensaje esta vacio");
-		} else {
-			agregarBorde("correcto", input);
-		}
-		bgSubmit();
-	});
-	btnReset.addEventListener("click", resetFormulario);
-	formulario.addEventListener("click", validarCampos);
-};
 cargarEventListeners();
+
+document.addEventListener("DOMContentLoaded", iniciarApp);
